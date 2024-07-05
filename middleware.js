d23a33c94429cba63199d8b55fe6bd66e002e85d
UdreaLucas <udrealucas@gmail.com>
2024-07-05 15:28:49 -0300

    trying to fix the middleware

--- middleware.js
+++ middleware.js
@@ -6,9 +6,10 @@ export default withAuth(function middleware(req) {
   console.log(req.nextauth.token.role);
   console.log(req.nextUrl.pathname);
 
-  if (req.nextauth.token.role === "Admin") {
-    return NextResponse.next();
-  } else {
+  if (
+    req.nextUrl.pathname.startsWith("/app/TicketPage") &&
+    req.nextauth.token.role !== "Admin"
+  ) {
     return NextResponse.rewrite(new URL("/unauthorized", req.nextUrl));
   }
 });
